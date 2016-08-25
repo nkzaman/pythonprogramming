fe03c506d059b60042b56b2c77577154690a6760
khranjan <004229640@coyote.csusb.edu>
2016-08-25 10:52:41 -0700

    file aded

---
+++ Convert-slasht to comma/convert-tabtodelimitedcomma.py
@@ -0,0 +1,12 @@
+import os
+import re
+cwd = os.getcwd() + '\\test'
+allfiles = os.listdir(cwd)
+for i in range(len(allfiles)):
+    fileobj  = open('test/'+ allfiles[i], 'r')
+    contents = fileobj.read();
+    newcontents= re.sub(r'\\t', ',' , contents)
+    fileobj2 = open('result/' + allfiles[i], 'w')
+    fileobj2.write(newcontents)
+    fileobj.close()
+
---
+++ Convert-slasht to comma/run.bat
@@ -0,0 +1,1 @@
+python convert-tabtodelimitedcomma.py
